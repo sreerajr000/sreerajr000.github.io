293edde6928d44e98f55a3f71a4a2870f13bc1f5
Sreeraj R <sreerajr000@gmail.com>
2017-07-16 23:35:13 +0530

    added code prettify

--- extras/Competitive Programming/bfs.cpp
+++ extras/Competitive Programming/bfs.cpp
@@ -4,9 +4,11 @@
 #define GRAY	1
 #define BLACK	2
 #define NIL	-1
+
 using namespace std;
 
 class Vertex {
+public:
 	int color;
 	int d;
 	int parent;
@@ -18,11 +20,37 @@ class Vertex {
 };
 
 
+vector<list<int> > adj;
+vector<Vertex> vertices;
+
+void BFS(int src) {
+	vertices[src].color = GRAY;
+	vertices[src].d = 0;
+	vertices[src].parent = NIL;
+
+	queue<int> Q;
+	Q.push(src);
+	while(!Q.empty()) {
+		int u = Q.front();
+		Q.pop();
+		cout << u << " ";
+		for(auto v: adj[u]){
+			if(vertices[v].color == WHITE){
+				vertices[v].color = GRAY;
+				vertices[v].d = vertices[u].d + 1;
+				vertices[v].parent = u;
+				Q.push(v);
+			}
+		}
+		vertices[u].color = BLACK;
+	}
+}
+
 int main() {
-	vector<list> adj;
 	int V, E;
 	cin >> V >> E;
 	adj.resize(V);
+	vertices.resize(V);
 
 	for(int i = 0; i < E; i++){
 		int u, v;
@@ -30,4 +58,8 @@ int main() {
 		adj[u].push_back(v);
 		adj[v].push_back(u);
 	}
+
+	int src;
+	cin >> src;
+	BFS(src);
 }
