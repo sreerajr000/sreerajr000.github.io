846810b4f2dd2e8d7cb792f768565546c1239805
Sreeraj R <sreerajr000@gmail.com>
2017-07-18 16:52:21 +0530

    added dijkstra

--- extras/Competitive Programming/dijkstra.cpp
+++ extras/Competitive Programming/dijkstra.cpp
@@ -24,19 +24,18 @@ void dijkstra(int s){
 		if (d == dist[u]){ // This check is important! We want to process vertex u only once but we can
 		// actually enqueue u several times in priority_queue... Fortunately, other occurrences of u
 		// in priority_queue will have greater distances and can be ignored (the overhead is small)
-
 			for(auto it : adj[u]) { // all outgoing edges from u
 				int v = it.first, weight_u_v = it.second;
 				if (dist[u] + weight_u_v < dist[v]) { // if can relax
 					dist[v] = dist[u] + weight_u_v; // relax
-					cout << v << " ";
 					pq.push(ii(dist[v], v)); // enqueue this neighbor
 				}   
 			} 
 
 		}
 	}                          // regardless whether it is already in pq or not
-
+	for(auto x: dist)
+		cout << x << " ";
 }
 
 
